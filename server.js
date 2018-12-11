17ee4e1e72652606a4466c118acb2544481e6b71
Carlos Lantigua <clantigua954@gmail.com>
2018-12-11 17:10:23 -0500

    fixed route bugs

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const server = express();
 
 // DB Config
@@ -8,13 +12,18 @@ const db = require('./config/keys').mongoURI;
 // Connect to mongoDB through mongoose
 mongoose
 	.connect(db, { useNewUrlParser: true })
-	.then(() => console.log('MongoDB Connect'))
+	.then(() => console.log('MongoDB Connected'))
 	.catch((err) => console.log(err));
 
 server.use(express.json());
 server.get('/', (req, res) => {
 	res.send({ message: 'server is alive' });
 });
+
+//////// Use Routes /////////////
+server.use('/api/users', users);
+server.use('/api/profile', profile);
+server.use('/api/posts', posts);
 
 const port = process.env.PORT || 9000;
 server.listen(port, () => {

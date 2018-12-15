9c8112e849fa5163e218d4bb55213d7b11ef303a
Carlos Lantigua <clantigua954@gmail.com>
2018-12-15 17:46:05 -0500

    created unlike post to server side route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -59,6 +59,50 @@ router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, re
 			.catch((err) => res.status(404).json({ message: 'post not found', err }));
 	});
 });
+
+// @route POST api/posts/like/:id
+// @desc Like a post
+// @access Private
+router.post('/like/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+	Profile.findOne({ user: req.user.id }).then((profile) => {
+		Post.findById(req.params.id)
+			.then((post) => {
+				if (post.likes.filter((like) => like.user.toString() === req.user.id).length > 0) {
+					return res.status(400).json({ message: 'User already liked this post' });
+				}
+
+				// Add user id to likes array
+				post.likes.unshift({ user: req.body.id });
+				// save to the database
+				post.save().then((post) => res.json(post));
+			})
+			.catch((err) => res.status(404).json({ message: 'post not found', err }));
+	});
+});
+
+// @route POST api/posts/unlike/:id
+// @desc Unlike a post
+// @access Private
+router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+	Profile.findOne({ user: req.user.id }).then((profile) => {
+		Post.findById(req.params.id)
+			.then((post) => {
+				if (post.likes.filter((like) => like.user.toString() === req.user.id).length === 0) {
+					return res.status(400).json({ message: 'User has not liked this post' });
+				}
+
+				// Get remove index
+				const removeIndex = post.likes.map((item) => item.user.toString()).indexOf(req.user.id);
+
+				// Splice out of the array
+				post.likes.splice(removeIndex, 1);
+				//save
+				post.save().then((post) => res.json(post));
+			})
+			.catch((err) => res.status(404).json({ message: 'post not found', err }));
+	});
+});
+
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
 	const { errors, isValid } = validatePostInput(req.body);
 

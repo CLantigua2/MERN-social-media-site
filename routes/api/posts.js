d96fb6af2481724cd35fb92c18aceb1b7d17f260
Carlos Lantigua <clantigua954@gmail.com>
2018-12-15 16:14:28 -0500

    created Post validation file

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,8 +3,26 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Post model
+const Post = require('../../models/Post');
+// Validation
+
 router.get('/test', (req, res) => {
 	res.send(200).json({ message: 'Posts works' });
+});
+
+// @route POST api/posts
+// @desc Create post
+// @access Private
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+	const newPost = new Post({
+		text: req.body.text,
+		name: req.body.name,
+		avatar: req.body.name,
+		user: req.user.id
+	});
+
+	newPost.save().then((post) => res.json(post));
 });
 
 module.exports = router;

c3660b11db9509919b720a4a8868e9fea320b52b
Carlos Lantigua <clantigua954@gmail.com>
2018-12-15 16:20:28 -0500

    setup post route for user post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,7 @@ const passport = require('passport');
 // Post model
 const Post = require('../../models/Post');
 // Validation
+const validatePostInput = require('../../validation/post');
 
 router.get('/test', (req, res) => {
 	res.send(200).json({ message: 'Posts works' });
@@ -15,14 +16,25 @@ router.get('/test', (req, res) => {
 // @desc Create post
 // @access Private
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+	const { errors, isValid } = validatePostInput(req.body);
+
+	// Check validation
+	if (!isValid) {
+		// if any errors, send 400 with errors object
+		return res.status(400).json(errors);
+	}
 	const newPost = new Post({
 		text: req.body.text,
 		name: req.body.name,
-		avatar: req.body.name,
+		avatar: req.body.avatar,
 		user: req.user.id
 	});
 
 	newPost.save().then((post) => res.json(post));
 });
+
+// @route POST api/posts
+// @desc Create post
+// @access Private
 
 module.exports = router;

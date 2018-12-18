84f33dea5cf0fda16c5c5eecc5719062a64e479c
Carlos Lantigua <clantigua954@gmail.com>
2018-12-18 10:25:54 -0500

    setup redux profile cases

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import errorReducer from './errorReducer';
+import profileReducer from './profileReducer';
 
 // takes in other reducers and combines
 export default combineReducers({
 	auth: authReducer,
-	errors: errorReducer
+	errors: errorReducer,
+	profile: profileReducer
 });
 
 // this file is used to take in all of the separate reducers and combine them then export them as an object

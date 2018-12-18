7dce903fe33b2d65ae93f7cf9ddd089322844c94
Carlos Lantigua <clantigua954@gmail.com>
2018-12-17 19:27:43 -0500

    setup login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
+import { loginUser } from '../../redux/actions';
 
 class Login extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			email: '',
-			password: ''
+			password: '',
+			errors: {}
 		};
 	}
 
@@ -24,6 +29,7 @@ class Login extends Component {
 	};
 
 	render() {
+		const { email, password, errors } = this.state;
 		return (
 			<div className="login">
 				<div className="container">
@@ -36,23 +42,29 @@ class Login extends Component {
 									<input
 										autoComplete="email"
 										type="email"
-										className="form-control form-control-lg"
+										className={classnames('form-control form-control-lg', {
+											'is-invalid': errors.email
+										})}
 										placeholder="Email Address"
 										name="email"
-										value={this.state.email}
+										value={email}
 										onChange={this.handleChange}
 									/>
+									{errors.email && <div className="invalid-feedback">{errors.email}</div>}
 								</div>
 								<div className="form-group">
 									<input
 										autoComplete="password"
 										type="password"
-										className="form-control form-control-lg"
+										className={classnames('form-control form-control-lg', {
+											'is-invalid': errors.password
+										})}
 										placeholder="Password"
 										name="password"
-										value={this.state.password}
+										value={password}
 										onChange={this.handleChange}
 									/>
+									{errors.password && <div className="invalid-feedback">{errors.password}</div>}
 								</div>
 								<input type="submit" className="btn btn-info btn-block mt-4" />
 							</form>
@@ -64,4 +76,15 @@ class Login extends Component {
 	}
 }
 
-export default Login;
+Login.propTypes = {
+	loginUser: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
+	errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+	auth: state.auth,
+	errors: state.errors
+});
+
+export default connect(mapStateToProps, { loginUser })(Login);
